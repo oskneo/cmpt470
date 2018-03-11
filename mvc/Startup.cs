f91682fd3ae4d7593a75ff6612d46fd9c6c5ec3f
osk666 <osk666@gmail.com>
2018-03-11 02:34:46 +0000

    pg

--- mvc/Startup.cs
+++ mvc/Startup.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 
 using Microsoft.EntityFrameworkCore;
-using test_net_core_mvc.Models.DataBase;
+using mvc.Models.DataBase;
 
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
