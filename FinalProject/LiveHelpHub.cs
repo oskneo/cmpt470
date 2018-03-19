2cfaa07a9ef2b10e808b95a79f63110efca2c713
osk666 <osk666@gmail.com>
2018-03-19 15:45:40 +0000

    p1

---
+++ FinalProject/LiveHelpHub.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ExploreCalifornia
+{
+    public class LiveHelpHub : Hub
+    {
+        public async Task SendMessage(string message)
+        {
+            //Clients.All.InvokeAsync(username, message);
+            //Clients.All.InvokeAsync("broadcastMessage", username, message);
+            //Groups.Add(Context.ConnectionId, "Employees");
+            //Clients.Group("Employee").showMessage(username, "This is a message for the Employees group.");
+            
+            //Clients.Caller.showMessage("system", "Your message was sent at " + DateTime.Now.ToString("h:mm tt"));
+            
+            
+            await Clients.All.InvokeAsync("Send", message);
+
+        }
+    }
+}
